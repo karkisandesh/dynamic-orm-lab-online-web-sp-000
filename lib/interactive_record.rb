fdb2284e940725406fdf1c2d8c0e4ba94e35712b
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-28 17:37:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,31 +7,32 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
   
-  # def self.column_names
-  #   DB[:conn].results_as_hash = true
-    
-  #   sql = "pragma table_info('#{table_name}')"
-  #   table_info = DB[:conn].execute(sql)
-    
-  #   column_names = []
-    
-  #   table_info.each do |column|
-  #     column_names << column["name"]
-  #   end
-  # end
-  
   def self.column_names
     DB[:conn].results_as_hash = true
-
+    
     sql = "pragma table_info('#{table_name}')"
-
     table_info = DB[:conn].execute(sql)
+    
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+    
+    table_info.each do |column|
+      column_names << column["name"]
     end
-    column_names.compact
   end
+  
+  # def self.column_names
+  #   DB[:conn].results_as_hash = true
+
+  #   sql = "pragma table_info('#{table_name}')"
+
+  #   table_info = DB[:conn].execute(sql)
+  #   column_names = []
+  #   table_info.each do |row|
+  #     column_names << row["name"]
+  #   end
+  #   column_names.compact
+  # end
+
    def initialize(options={})
       options.each do |property, value|
         self.send("#{property}=", value)

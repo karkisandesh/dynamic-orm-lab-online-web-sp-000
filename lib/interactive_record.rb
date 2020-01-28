800c5e4ceb1ced1ef31f784c9f825beb1636eacc
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-28 17:23:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,6 +40,6 @@ class InteractiveRecord
   end
   
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert") VALUES (?)", 
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert") VALUES (?)", [values_for_insert])
   end
 end

d84a1e17368e54b089fcb905667107a1651e34e0
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-28 17:15:47 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,8 +28,9 @@ class InteractiveRecord
   end
   
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == id}
-    
+    self.class.column_names.delete_if {|col| col == "id"}
   end
+  
+  
   
 end

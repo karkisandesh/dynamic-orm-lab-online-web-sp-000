bbea661ffd6fcc554b6be208ba46fed83b938142
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-28 17:14:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,11 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
+    def initialize(options={})
+      options.each do |property, value|
+        self.send("#{property}=", value)
+      end
+    end
 
 end
